b86ba968f59364000c9f9b0141b9ba557cb54604
giovanna <gslima753@gmail.com>
2026-08-19 20:41:57 -0300

    Fix file paths in CONFIG object

--- script.js
+++ script.js
@@ -18,8 +18,8 @@
 ============================================================ */
  
 const CONFIG = {
-    avatar: './files(1)/Avatar CAIMENT.png',
-    calca: './files(1)/calca.png',
+    avatar: './files (1)/Avatar CAIMENT.png',
+    calca: './files (1)/calca.png',
     whatsapp: '5511999999999'
 };
    
