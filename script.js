15ac21e5b73de84fd12ecbc3ac796c21eb273fe9
giovanna <gslima753@gmail.com>
2026-08-19 20:29:53 -0300

    Fix file paths for avatar and calca images (javaScriptv2)

--- script.js
+++ script.js
@@ -18,8 +18,8 @@
 ============================================================ */
  
 const CONFIG = {
-    avatar: './fiels/Avatar CAIMENT.png',
-    calca: './fiels/calca.png',
+    avatar: './fiels(1)/Avatar CAIMENT.png',
+    calca: './fiels(1)/calca.png',
     whatsapp: '5511999999999'
 };
    
